b75a2af12cca2099bc340a3f4e524432c86f91ad
GabrielTeodoroDomingues <gabrielteodoron642@gmail.com>
2020-01-24 11:56:35 -0300

    Finalizando a API de profissionais

--- backend/app/Http/Controllers/ProfissionalController.php
+++ backend/app/Http/Controllers/ProfissionalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Profissional;
+use App\Tecnologia;
 
 class ProfissionalController extends Controller
 {
@@ -13,11 +14,17 @@ class ProfissionalController extends Controller
         return response()->json($listaProfissionais);
     }
     public function criarProfissional(Request $request){
+        $tecnologia_ID = $request->tecnologia;
         $newProfissional = new Profissional();
         $newProfissional->nome = $request->nome;
         $newProfissional->github = $request->github;
         $result = $newProfissional->save();
-
+        $tecnologia = Tecnologia::find($tecnologia_ID);
+        if($tecnologia){
+            $tecnologia->profissional()->attach($newProfissional->id);
+        }else {
+            return response()->json(['error'=>'O id da tecnologia não existe']);
+        }
         return response()->json($newProfissional);
     }
 }
